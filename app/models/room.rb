08f7c821d4e03181de925761e9f9f10a9ebeeb6c
Karol Ostrowski <lolson@lolson-pop-os.home>
2020-10-14 21:10:52 +0200

    Simple videochat app

---
+++ app/models/room.rb
@@ -0,0 +1,9 @@
+class Room < ApplicationRecord
+  # noinspection RubyResolve
+  before_create do 
+    opentok = OpenTok::OpenTok.new Rails.application.credentials.vonage_api_key,
+                                   Rails.application.credentials.vonage_api_secret
+    session = opentok.create_session
+    self.vonage_session_id = session.session_id
+  end
+end
