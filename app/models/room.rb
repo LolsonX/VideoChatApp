bfbb714ff7458f21cc40cf2e90122eec61c29d97
Karol Ostrowski <xlolsonx@gmail.com>
2020-11-29 11:38:23 +0100

    Better GUI

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,12 @@
 class Room < ApplicationRecord
   # noinspection RubyResolve
   before_create do
+    opentok_session
+  end
+
+  private
+
+  def opentok_session
     opentok = OpenTok::OpenTok.new ENV.fetch('VONAGE_API_KEY', Rails.application.credentials.vonage_api_key),
                                    ENV.fetch('VONAGE_API_SECRET', Rails.application.credentials.vonage_api_secret)
     session = opentok.create_session
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'dashboard/index'
-  root to: 'dashboard#index'
+  root to: 'rooms#index'
   devise_for :users
   resources :rooms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
